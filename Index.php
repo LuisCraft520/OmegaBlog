67a9dbce79a1db494240750c8daf14fd64092f9d
LouisOlouis <lcoliveiraaguiar@gmail.com>
2025-10-09 16:22:00 -0300

    Novo nome

--- Index.php
+++ Index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$USUARIO = isset($_SESSION['usuario']) ? null : $_SESSION['usuario'];
+$USUARIO = isset($_SESSION['usuario']) ? $_SESSION['usuario'] : null ;
 
 $invisivel_prelogin = $USUARIO ? 'style="display: none;"' : '';
 $invisivel_poslogin = $USUARIO ? '' : 'style="display: none;"';
@@ -10,20 +10,30 @@ $invisivel_poslogin = $USUARIO ? '' : 'style="display: none;"';
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>OmegaBlog</title>
+    <title>OmegaOn</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header class="Top">
-        <div class="Container">
-            <h2 class="title">OmegaBlog</h2>
-            <div class="Perfil">
-                <a class="Login-Button" href="login.php" <?php echo $invisivel_prelogin; ?>>Login</a>
-                <h2 class="Nome" <?php echo $invisivel_poslogin; ?>>
-                    <?php echo htmlspecialchars($USUARIO); ?>
-                </h2>
-            </div>
+<header class="Top">
+    <div class="Container">
+        <h2 class="title">OmegaOn</h2>
+        <div class="Perfil">
+            <a class="Login-Button" href="login.php" <?php echo $invisivel_prelogin; ?>>Login</a>
+            <h2 class="Nome" <?php echo $invisivel_poslogin; ?>>
+                <?php echo htmlspecialchars($USUARIO); ?>
+            </h2>
         </div>
-    </header>
+    </div>
+</header>
+<div class="Body">
+    <div class="left"></div>
+    <div class="middle">
+        <br>
+        <h1>Bem vindo ao OmegaOn</h1>
+        <h3>O forum oficial da turma omega</h3>
+        <br>
+    </div>
+
+</div>
 </body>
 </html>
